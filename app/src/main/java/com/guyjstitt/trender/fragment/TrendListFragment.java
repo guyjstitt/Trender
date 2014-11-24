640df773bb875db8f33670c29190641a56f4e35a
Michael Houze <michael.houze@gmail.com>
2014-11-24 15:09:00 -0500

    Create fragment package
    - Move fragment classes to new package

--- app/src/main/java/com/guyjstitt/trender/fragment/TrendListFragment.java
+++ app/src/main/java/com/guyjstitt/trender/fragment/TrendListFragment.java
@@ -1,6 +1,8 @@
-package com.guyjstitt.trender;
+package com.guyjstitt.trender.fragment;
 
 import android.app.Fragment;
+
+import com.guyjstitt.trender.GlobalState;
 
 /**
  * Created by Michael on 11/24/2014.
